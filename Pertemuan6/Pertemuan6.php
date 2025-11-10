9042d95d3836a3b4f60c0b8c520129803b803892
KudaJambu <syahruls90807512@gmail.com>
2025-11-10 09:45:29 +0700

    Add files via upload

---
+++ Pertemuan6/Pertemuan6.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+</style>
+<body>
+    <?php
+        // echo "WarGlory <br>";
+        // echo "Bidzyx <br>";
+        // echo "Labyrinth 18F Beater <br>";
+
+        // $a = "15";
+        // $b = 7;
+        // $c = $a + $b;
+        // echo "Hasil Penjumlahan $a dengan $b adalah $c <br>";
+        // $c = $a - $b;
+        // echo "Hasil Pengurangan $a dengan $b adalah $c <br>";
+        // $c = $a * $b;
+        // echo "Hasil Perkalian $a dengan $b adalah $c <br>";
+        // $c = $a / $b;
+        // echo "Hasil Pembagian $a dengan $b adalah $c <br>";
+        // $c = $a % $b;
+        // echo "Sisa Pembagian $a dengan $b adalah $c <br>";
+        // echo gettype($a);
+        // echo gettype($b);
+
+        // $nilai = 70;
+        // if ($nilai >= 80){
+        //     echo "Nilai Anda $nilai Peringkat A";
+        // } else if ($nilai >= 70){
+        //     echo "Nilai Anda $nilai Peringkat B";
+        // } else {
+        //     echo "Nilai Anda $nilai Peringkat C";
+        // }
+
+        // $lampu = "Kuning";
+        // switch ($lampu){
+        //     case "Merah":
+        //         echo "Stop!";
+        //         break;
+        //     case "Kuning":
+        //         echo "Hati-hati";
+        //         break;
+        //     case "Hijau":
+        //         echo "Jalan";
+        //         break;
+        //     default:
+        //         echo "Tidak ada warna yang cocok";
+        // }
+
+        // for ($i=1; $i<=10; $i++){
+        //     echo "$i<br>";
+        // }
+
+        // $i = 1;
+        // while ($i<=3){
+        //     echo "$i<br>";
+        //     $i++;
+        // }
+        
+        // $i = 1;
+        // do{
+        //     echo "$i<br>";
+        //     $i++;
+        // }
+        // while ($i<=5);
+
+        // $data = array(1,3,5,7);
+        // foreach($data as $tampil){
+        //     echo "$tampil <br>";
+        // }
+
+    ?>
+    
+</body>
+</html>
